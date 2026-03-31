aa52cd7e031e17f218bc8d2d72f817d7806b3c8b
sakshi07bhosale <sakshibhosale749@gmail.com>
2026-03-31 21:08:40 +0530

    some changes done

--- Desktop/RTC/React  js/Assi-26/src/main.jsx
+++ Desktop/RTC/React  js/Assi-26/src/main.jsx
@@ -1,21 +1,20 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes,Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./views/Home/Home";
 import About from "./views/About/About";
 import Contact from "./views/Contact/Contact";
 import OurServices from "./views/OurServices/OurServices";
 import NotFound from "./views/NotFound/NotFound";
 
-const root =  createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/About" element={<About />} />
-    <Route path="/Contact" element={<Contact />} />
-    <Route path="/OurServices" element={<OurServices />} />
-    <Route path="*" element={<NotFound />} />
-
-  </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/ourservices" element={<OurServices />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   </BrowserRouter>
 );
